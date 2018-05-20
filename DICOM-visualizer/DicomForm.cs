54ba6906e5834dd2f3d8e00bf21212337943880e
Maciej Krawczyk <krawczykmf@gmail.com>
2018-05-20 20:48:18 +0200

    create basic layout for DICOM viewer

---
+++ DICOM-visualizer/DicomForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DICOM_visualizer
+{
+    public partial class DicomForm : Form
+    {
+        public DicomForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void minimalValueTrackbar_Scroll(object sender, EventArgs e)
+        {
+            TrackBar trackBar = sender as TrackBar;
+            trackBar.Value = Math.Min(trackBar.Value, maximalValueTrackbar.Value);
+            minimalValueLabel.Text = trackBar.Value.ToString();
+        }
+
+        private void maximalValueTrackbar_Scroll(object sender, EventArgs e)
+        {
+            TrackBar trackBar = sender as TrackBar;
+            trackBar.Value = Math.Max(trackBar.Value, minimalValueTrackbar.Value);
+            maximalValueLabel.Text = trackBar.Value.ToString();
+        }
+
+        private void loadDicomButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "DICOM files | *.dcm";
+            openFileDialog.ShowDialog();
+        }
+    }
+}
--- DICOM-visualizer/Program.cs
+++ DICOM-visualizer/Program.cs
@@ -16,7 +16,7 @@ namespace DICOM_visualizer
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new DicomForm());
         }
     }
 }
